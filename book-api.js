1f9c4c0438c22d45c0f0cedfb2c11b5e7a999a10
Marlen-Valdez <marlenvaldez406@gmail.com>
2021-03-04 22:48:45 -0600

    first commit

---
+++ book-api.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+app.delete('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Remove item from the books array
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('Book is deleted');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.get('/books', (req, res) => {
+    res.json(books);
+});
